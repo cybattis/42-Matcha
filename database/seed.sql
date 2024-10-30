5d87c1e50cfb0f7256957c8a7ab3d4c10d4f216d
Matthieu Servagent <mservage@z3r9p5.42lyon.fr>
2024-10-30 13:25:33 +0100

    adding init and seed database

---
+++ database/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO tags (name) VALUES ('Sports'),('Video Games'), ('Music'), ('Reading'), ('Hikes');
+
+INSERT INTO gender (name) VALUES ('Hetero'), ('Homosexual'), ('Bisexual');
+
+INSERT INTO `status`(name) VALUES ('Pending'), ('Sent'), ('Delivered'), ('Read'), ('Error'), ('Edited');
