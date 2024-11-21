0d18d5950a3562302473b815e5fd8c2bfbe5d384
Cyril Battistolo <cybattis@student.42lyon.fr>
2024-11-21 10:05:52 +0100

    WIP: Update profile creation in DB (#10)
    * Update database user models
    Update userProfile controller
    Add stored procedures for user profile
    refactor and clean project file
    
    * Update Tags model and user profile procedure
    Update Tags to profile creation

--- backend/Models/Users/UserProfile.cs
+++ backend/Models/Users/UserProfile.cs
@@ -9,26 +9,18 @@ public class UserProfile
     public string? FirstName {get;set;}
     [DefaultValue("Bono")]
     public string? LastName {get;set;}
-    [DefaultValue("0")]
-    public int? GenderId {get;set;} // 0: Men 1: Women
-    [DefaultValue("0")]
+    [DefaultValue("1")]
+    public int? GenderId {get;set;} // 1: Men 2: Women
+    [DefaultValue("1")]
     public int? SexualOrientation {get;set;}
     [DefaultValue("This is a bio.")]
     public string? Biography {get;set;}
     [DefaultValue("Lyon")]
     public string? Localisation {get;set;}
     
-    // Pictures
-    public string? Picture1 {get;set;}
-    public string? Picture2 {get;set;}
-    public string? Picture3 {get;set;}
-    public string? Picture4 {get;set;}
-    public string? Picture5 {get;set;}
-    
     // Tags
-    public int? Tag1 {get;set;}
-    public int? Tag2 {get;set;}
-    public int? Tag3 {get;set;}
-    public int? Tag4 {get;set;}
-    public int? Tag5 {get;set;}
+    public List<int> Tags {get;set;} = new();
+    
+    // Pictures
+    public List<string> Pictures {get;set;} = new();
 }
--- database/stored_procedure/sp_users.sql
+++ database/stored_procedure/sp_users.sql
@@ -15,7 +15,8 @@ END //
 # GetUserProfile
 CREATE PROCEDURE GetUserProfile(IN userID INT)
 BEGIN
-    SELECT first_name, last_name, gender_id, sexual_orientation, biography, profile_completion_percentage, localisation  FROM users WHERE id = userID;
+    SELECT first_name, last_name, gender_id, sexual_orientation, biography, profile_completion_percentage, localisation FROM users WHERE id = userID;
+    SELECT * FROM users_tags WHERE user_id = userID;
 END //
 
 # CreateUserProfile
@@ -26,7 +27,12 @@ CREATE PROCEDURE CreateUserProfile(
     IN _gender_id INT,
     IN _sexual_orientation INT,
     IN _localisation VARCHAR(100),
-    IN _biography VARCHAR(250)
+    IN _biography VARCHAR(250),
+    IN _tag1 INT,
+    IN _tag2 INT,
+    IN _tag3 INT,
+    IN _tag4 INT,
+    IN _tag5 INT
 )
 BEGIN
     UPDATE users
@@ -37,6 +43,42 @@ BEGIN
         localisation = _localisation, 
         biography = _biography 
     WHERE id = userID;
+    
+    IF _tag1 IS NOT NULL THEN
+        INSERT INTO users_tags (user_id, tag_id)
+        VALUES (userID, _tag1)
+        ON DUPLICATE KEY UPDATE
+            tag_id = VALUES(tag_id);
+    END IF;
+    
+    IF _tag2 IS NOT NULL THEN
+        INSERT INTO users_tags (user_id, tag_id)
+        VALUES (userID, _tag2)
+        ON DUPLICATE KEY UPDATE
+            tag_id = VALUES(tag_id);
+    END IF;
+    
+    IF _tag3 IS NOT NULL THEN
+        INSERT INTO users_tags (user_id, tag_id)
+        VALUES (userID, _tag3)
+        ON DUPLICATE KEY UPDATE
+            tag_id = VALUES(tag_id);
+    END IF;
+    
+    IF _tag4 IS NOT NULL THEN
+        INSERT INTO users_tags (user_id, tag_id)
+        VALUES (userID, _tag4)
+        ON DUPLICATE KEY UPDATE
+            tag_id = VALUES(tag_id);
+    END IF;
+    
+    IF _tag5 IS NOT NULL THEN
+        INSERT INTO users_tags (user_id, tag_id)
+        VALUES (userID, _tag5)
+        ON DUPLICATE KEY UPDATE
+            tag_id = VALUES(tag_id);
+    END IF;
+        
 END //
 
 DELIMITER ;
