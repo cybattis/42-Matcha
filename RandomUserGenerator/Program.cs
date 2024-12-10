41232e4cc4e3c49308c625ce4dd753f190c8e4b0
Cyril Battistolo <cybattis@student.42lyon.fr>
2024-12-10 16:59:10 +0100

    UserGenerator: add number of each sex to user.

--- RandomUserGenerator/Program.cs
+++ RandomUserGenerator/Program.cs
@@ -5,6 +5,15 @@ var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING"
                        "Server=localhost;Port=3307;Database=db;user=user;password=password;";
 var imagesPath = "/home/cybattis/Dev/42/Matcha/backend/images/";
 
+// Ask for number of male and female users to generate
+Console.WriteLine("Welcome to Random User Generator!");
+
+Console.Write("Enter the number of female users to generate: ");
+var numberOfFemaleUsers = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Enter the number of male users to generate: ");
+var numberOfMaleUsers = int.Parse(Console.ReadLine() ?? "0");
+
 Console.WriteLine("Initializing...");
 
 // Check if file path exist
@@ -16,11 +25,11 @@ connection.Open();
 
 Console.WriteLine("Generating User Data...");
 Console.WriteLine("Generating female users...");
-await GenerateUser.Generate(connection, "female", 100, imagesPath);
+await GenerateUser.Generate(connection, "female", numberOfFemaleUsers, imagesPath);
 Console.WriteLine("Done!");
 
 Console.WriteLine("Generating male users...");
-await GenerateUser.Generate(connection, "male", 100, imagesPath);
+await GenerateUser.Generate(connection, "male", numberOfMaleUsers, imagesPath);
 Console.WriteLine("Done!");
 
 connection.Close();
